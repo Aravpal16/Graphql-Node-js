1cf12e7895d469c4fdaf8fee5950f96d499fc33b
Aravind <36320357+Aravpal16@users.noreply.github.com>
2020-10-27 09:47:07 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link
+}
+const prisma = new PrismaClient()
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
