586e0b5233faac61af959fcae19076f508a0e14d
Aravind <36320357+Aravpal16@users.noreply.github.com>
2020-12-01 11:27:46 +0530

    Update Mutation.js

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -44,17 +44,50 @@ async function login(parent, args, context, info) {
 function post(parent, args, context, info) {
   const userId = getUserId(context)
 
-  return context.prisma.link.create({
+  const newLink = context.prisma.link.create({
     data: {
       url: args.url,
       description: args.description,
       postedBy: { connect: { id: userId } },
     }
   })
+  context.pubsub.publish("NEW_LINK", newLink)
+
+  return newLink
+}
+async function vote(parent, args, context, info) {
+  // 1
+  const userId = getUserId(context)
+
+  // 2
+  const vote = await context.prisma.vote.findOne({
+    where: {
+      linkId_userId: {
+        linkId: Number(args.linkId),
+        userId: userId
+      }
+    }
+  })
+
+  if (Boolean(vote)) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+
+  // 3
+  const newVote = context.prisma.vote.create({
+    data: {
+      user: { connect: { id: userId } },
+      link: { connect: { id: Number(args.linkId) } },
+    }
+  })
+  context.pubsub.publish("NEW_VOTE", newVote)
+
+  return newVote
 }
 
 module.exports = {
   signup,
   login,
   post,
+  vote,
 }
